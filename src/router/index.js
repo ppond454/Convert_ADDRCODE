5b28f309d0f427e5f4123b2bcb0738b8b970cc70
Lilp <lilp@Lilps-iMac-Pro.local>
2022-03-17 12:52:27 +0700

    17/03/22

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '/src/views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import Home from "/src/views/Home.vue"
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: Home,
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+    meta: {
+      title: "Home",
+    //   icon: "../assets/thememint.png"
     },
+  },
 ]
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
+})
+
+router.beforeEach((to,from,next)=>{
+    document.title = `CaC | ${to.meta.title}`
+    // document.querySelector("[rel='icon']").setAttribute('href',to.meta.icon)
+    next()
 })
 
 export default router
